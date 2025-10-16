eb5f0db212a604c20b5d84b682340f718b75cff1
ksankeerth <shankeerthan1995@gmail.com>
2025-10-16 13:01:22 +0530

    [ui] added chip filter component

---
+++ webapp/src/components/ChipsFilter.tsx
@@ -0,0 +1,94 @@
+import { Checkbox } from "primereact/checkbox";
+import { Chip } from "primereact/chip";
+import { OverlayPanel } from "primereact/overlaypanel";
+import React, { useRef, useState } from "react";
+
+export type ChipFilterOption = {
+  label: string;
+  value: string;
+};
+
+export type ChipFilterProps = {
+  filterOptions: ChipFilterOption[];
+};
+
+const ChipsFilter = (props: ChipFilterProps) => {
+  const opRef = useRef<OverlayPanel>(null);
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+  const [selectedOptions, setSelectedOptions] = useState<Set<string>>(
+    new Set(props.filterOptions.map((op) => op.value))
+  );
+
+  return (
+    <React.Fragment>
+      {/*  border-1 border-solid border-round-lg border-teal-100 */}
+      <div
+        className="p-2 m-0
+        
+        flex justify-content-between align-items-center
+        hover:bg-white hover:border-1"
+        onClick={(e) => {
+          opRef.current?.toggle(e);
+        }}
+      >
+        {!showOptions && (
+          <span
+            className="pi pi-chevron-down  text-xs pr-2 pl-2"
+            onClick={() => setShowOptions((c) => !c)}
+          />
+        )}
+        {showOptions && (
+          <span
+            className="pi pi-chevron-up  text-xs pr-2 pl-2"
+            onClick={() => setShowOptions((c) => !c)}
+          />
+        )}
+
+        <div className="flex ">
+          {selectedOptions.size != 0 &&
+            Array.from(selectedOptions).map((value: string) => {
+              return (
+                <Chip
+                  className="m-1 text-xs"
+                  label={
+                    props.filterOptions.find((op) => op.value == value)?.label
+                  }
+                />
+              );
+            })}
+        </div>
+
+        <OverlayPanel ref={opRef} className="p-0 m-0">
+          <div className="flex flex-column text-xs">
+            {props.filterOptions.map((node: ChipFilterOption) => (
+              <div className="flex flex-row gap-5 p-1 align-items-center">
+                <Checkbox
+                  value={node.value}
+                  checked={selectedOptions.has(node.value)}
+                  onChange={(e) => {
+                    if (e.checked) {
+                      setSelectedOptions((current) => {
+                        const newSet = new Set(current);
+                        newSet.add(e.value);
+                        return newSet;
+                      });
+                    } else {
+                      setSelectedOptions((current) => {
+                        const newSet = new Set(current);
+                        newSet.delete(e.value);
+                        return newSet;
+                      });
+                    }
+                  }}
+                />
+                <div>{node.label}</div>
+              </div>
+            ))}
+          </div>
+        </OverlayPanel>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ChipsFilter;
